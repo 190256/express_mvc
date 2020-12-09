3d26223cf8f59cabadf9db1fd0c08d6be52e3d5f
190256 <190256hr@yse-c.net>
2020-12-09 12:09:26 +0900

    Second

---
+++ controllers/HomeController.js
@@ -0,0 +1,8 @@
+// アクションメソッド
+exports.index = (req, res) => {
+    res.render('index.ejs', { title: 'Index Page' })
+}
+
+exports.about = (req, res) => {
+    res.render('about.ejs', { title: 'About Page' })
+}
